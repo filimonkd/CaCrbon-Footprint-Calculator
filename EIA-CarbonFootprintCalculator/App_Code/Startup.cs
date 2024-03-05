acb26d3b1ae3f2034f85351a05ef181445a84bd6
= <=>
2024-03-05 10:35:43 +0300

    add files

---
+++ EIA-CarbonFootprintCalculator/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EIA_CarbonFootprintCalculator.Startup))]
+namespace EIA_CarbonFootprintCalculator
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
